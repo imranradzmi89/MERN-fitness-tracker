20afe02a532db1e0377ccc533fb007c296e8d00b
Imran Radzmi <imranradzmi89@gmail.com>
2021-09-20 19:19:55 +0800

    git ignore

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 export default class CreateUser extends Component {
 constructor(props) {
@@ -27,7 +28,8 @@ onSubmit(e) {
         }
 
         console.log(username)
-
+        axios.post('http://localhost:5000/users/add' , username)
+        .then( res => console.log(res.data))
         this.setState({
             username: ''
         })
